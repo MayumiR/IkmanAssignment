f131fcc31143615ef7ccedfc0d9b4cece4da8b18
Datamation-systems <dsplandroid@gmail.com>
2019-08-19 15:17:04 +0530

    first commit

---
+++ app/src/main/java/info/ikman/recyclerviewsearch/Contact.java
@@ -0,0 +1,26 @@
+package info.ikman.recyclerviewsearch;
+
+/**
+ * Created by rashmi
+ */
+
+public class Contact {
+    String title;
+    String image;
+    String description;
+
+    public Contact() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
